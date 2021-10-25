faf4b96a0069c9bc3096006bd91dea32d8a6a35d
kokhaur <85614260+kokhaur@users.noreply.github.com>
2021-10-25 15:03:56 +0800

    Update Step3_ClusterGLCMFeatureExtraction.m

--- Step3_ClusterGLCMFeatureExtraction.m
+++ Step3_ClusterGLCMFeatureExtraction.m
@@ -129,7 +129,7 @@ for iOffSet = 1 : length(offsets)
         for i=1:length(FileList)
             load([strPath FileList(i).name]);
             m_FeatureManager = [];
-            [m_FeatureManager] = funcLesionsFeatureExtractionTesting(arrImgFLAIR, arrImgAnnotated, arrImgLesionCandidate, m_FeatureManager, iLevel, offsets(iOffSet,:), iMethod);
+            [m_FeatureManager] = funcLesionsFeatureExtractionTrainingNonLesions(arrImgFLAIR, arrImgAnnotated, arrImgLesionCandidate, m_FeatureManager, iLevel, offsets(iOffSet,:), iMethod);
 
             for iIndex = 1 : length(m_FeatureManager)
              
